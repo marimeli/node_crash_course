170238ad9206e4c7c7c2f3a5e789a4ce263b7e87
marimeli <marimel.casas@gmail.com>
2019-10-07 00:26:41 -0500

    logger

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const Person = require('./person');
+/* const Person = require('./person');
 
 const person1 = new Person ('Ale', 24);
 
-person1.greeting();
+person1.greeting(); */
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
