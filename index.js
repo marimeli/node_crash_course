5d425a3e84cd499546948cbed0313cdef0afb748
marimeli <marimel.casas@gmail.com>
2019-10-09 11:25:33 -0500

    create server I

--- index.js
+++ index.js
@@ -18,16 +18,27 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-
 const server = http.createServer((req, res) => {
-  /*  console.log(req.url); // result --> / */
   if (req.url === '/') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html' //write header html
-    });
-    res.end('<h1>Homepage</h1>')
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
   }
-})
+
+  if (req.url === '/api/users') {
+    const users = [
+      { name: 'Bob Smith', age: 40 },
+      { name: 'John Doe', age: 30 }
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
